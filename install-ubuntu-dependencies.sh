c6bb41b8b560215f4cc5fcf9d01fa0c296d0aa37
Daniel Edgar <aknot242@gmail.com>
2019-09-09 13:08:31 -0700

    adding safety check on elastic config

---
+++ install-ubuntu-dependencies.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install software-properties-common ansible python-pip -y
+# sudo apt install jmespath
+sudo pip install jmespath
