122f2773ffe6a9f0ec38f1cc22835142f1585bba
Daniel Edgar <aknot242@gmail.com>
2019-09-09 16:23:33 -0700

    updating security

--- install-ubuntu-dependencies.sh
+++ install-ubuntu-dependencies.sh
@@ -1,10 +1,11 @@
 sudo apt update
 
-sudo apt install docker.io
+sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo usermod -a -G docker $USER
 
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install software-properties-common ansible python-pip -y
 
-sudo pip install jmespath docker docker-py docker-compose
+sudo pip install jmespath docker docker-compose
