79f0fc505cb0c3a0187a4e6689ddbd058367bb4e
Daniel Edgar <aknot242@gmail.com>
2019-09-09 15:48:27 -0700

    add dependencies

--- install-ubuntu-dependencies.sh
+++ install-ubuntu-dependencies.sh
@@ -1,5 +1,11 @@
 sudo apt update
+
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install software-properties-common ansible python-pip -y
-# sudo apt install jmespath
+
 sudo pip install jmespath
+sudo pip install docker docker-py docker-compose
