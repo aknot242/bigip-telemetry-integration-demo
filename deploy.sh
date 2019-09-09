bfd817867ce63a7961e596c59e151a15e27aa16f
Daniel Edgar <aknot242@gmail.com>
2019-09-09 11:33:37 -0700

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#start container
+# docker run -v $(pwd):/home/snops/big-ip-ts-integration -it --name big-ip-ts-integration --rm f5usecases/f5-rs-container
+
+ansible-playbook playbooks/big-ip-ts-integration.yml
