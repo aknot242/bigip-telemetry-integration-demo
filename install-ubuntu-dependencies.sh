879a513db6c3d4a91a7cd8362951467b3c811826
Daniel Edgar <aknot242@gmail.com>
2019-09-11 13:42:41 -0700

    rpm is needed to validate the LX rpm package contents (used by the ansible module)

--- install-ubuntu-dependencies.sh
+++ install-ubuntu-dependencies.sh
@@ -6,6 +6,6 @@ sudo systemctl enable docker
 #sudo usermod -a -G docker $USER
 
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install software-properties-common ansible python-pip -y
+sudo apt install software-properties-common ansible rpm python-pip -y
 
 sudo pip install jmespath docker docker-compose
