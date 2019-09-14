aed0ae25f98df9d87399ac36f3de7dd15e0a5375
Daniel Edgar <aknot242@gmail.com>
2019-09-14 11:14:13 -0700

    splitting playbooks. adding inventory file and setting container restart policies

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,1 @@
-#start container
-# docker run -v $(pwd):/home/snops/big-ip-ts-integration -it --name big-ip-ts-integration --rm f5usecases/f5-rs-container
-
-ansible-playbook ansible/playbooks/big-ip-ts-integration.yml
+ansible-playbook ansible/playbooks/site.yml
