39ec85e2e7d2660ae438aef26e28dc651f1898a9
Daniel Edgar <aknot242@gmail.com>
2019-09-14 13:37:30 -0700

    fixing addresses, adding interface to ubuntu server, forcing ansible to run local for linux server

--- install-ubuntu-dependencies.sh
+++ install-ubuntu-dependencies.sh
@@ -9,3 +9,10 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install software-properties-common ansible rpm python-pip -y
 
 sudo pip install jmespath docker docker-compose
+
+# create ssh keys for user
+# ssh-keygen -t rsa -b 4096
+# cat /home/ubuntu/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+# add internal IP
+sudo ifconfig eth1 10.1.10.11 netmask 255.255.255.0 broadcast 10.1.10.255
