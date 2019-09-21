4a481c1e0590c43650fac05f39c00e711c663711
Daniel Edgar <aknot242@gmail.com>
2019-09-21 08:06:42 -0700

    moving all possible unix server configuration to playbook

--- install-ubuntu-dependencies.sh
+++ install-ubuntu-dependencies.sh
@@ -1,18 +1,9 @@
+# bootstrap
 sudo apt update
-
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-#sudo usermod -a -G docker $USER
-
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install software-properties-common ansible rpm python-pip -y
-
-sudo pip install jmespath docker docker-compose
+sudo apt install software-properties-common ansible -y
+ansible-galaxy install -r ansible/requirements.yml
 
 # create ssh keys for user
 # ssh-keygen -t rsa -b 4096
 # cat /home/ubuntu/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-
-# add internal IP
-sudo ifconfig eth1 10.1.20.11 netmask 255.255.255.0 broadcast 10.1.20.255
