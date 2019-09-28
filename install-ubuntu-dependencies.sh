54680e02a4c61ae47db6ec0153a994454e1d272b
Daniel Edgar <aknot242@gmail.com>
2019-09-28 10:55:57 -0700

    wasn't suppressing restart prompting in the correct script

--- install-ubuntu-dependencies.sh
+++ install-ubuntu-dependencies.sh
@@ -1,9 +1,9 @@
-# bootstrap
+# prevent prompting for restart when installing core packages such as libssl
+DEBIAN_FRONTEND=noninteractive
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+
+# install dependencies
 sudo apt update
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install software-properties-common ansible -y
 ansible-galaxy install -r ansible/requirements.yml
-
-# create ssh keys for user
-# ssh-keygen -t rsa -b 4096
-# cat /home/ubuntu/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
